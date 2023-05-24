0f22a8e27bf5a7be42500b91f5f42be6795c988c
unknown <nelsonhg82@gmail.com>
2023-05-23 22:37:22 -0300

    segundo commit del parcial

--- main.py
+++ main.py
@@ -37,24 +37,60 @@ def mostrar_nombre_y_dato(lista:list, dato:str):
     for jugador in lista:
         print("Nombre: {0} - Posicion: {1}".format(jugador["nombre"], jugador[dato]))
         
-mostrar_nombre_y_dato(lista_jugadores, "posicion")
+#mostrar_nombre_y_dato(lista_jugadores, "posicion")
 
-def mostrar_indice_y_nombre (lista:list, dato:str):
+def mostrar_indice_y_nombre (lista:list)->bool:
     '''
-    - Muestra el nombre y el dato pasado por parametro.
-    - Recibe una lista, una constante para formatear el print y
-      y el dato.
-    - No retorna nada.
+    - Muestra un numero correspondiente al indice y el nombre del jugador.
+    - Recibe una lista.
+    - Retorna False si la lista esta vacia sino retorna True.
     '''
-    for jugador in lista:
-        print("Indice: {0} - Nombre: {1}".format(jugador["nombre"], jugador[dato]))
-
-
-def obtener_estadisticas_jugador (lista:list, indice:str)->list:
-    '''
-    - 
-    '''
-    retorno = None
+    retorno = False
+       
     if lista != []:
-        for jugador in lista:
-            
+        for indice in range(len(lista)):
+            print("Indice: {0} - Nombre: {1}".format(indice, lista[indice]["nombre"]))
+        
+        retorno = True
+        
+    return retorno
+    
+    
+#mostrar_indice_y_nombre(lista_jugadores)
+
+def mostrar_nombre_un_jugador (lista:list, indice:str):
+    '''
+    - Muestra el nombre de un jugador.
+    - Recibe una lista y el indice de la lista.
+    - Retorna False si la lista esta vacia sino retorna True.
+    '''
+    retorno = False
+    indice = int(indice)
+       
+    if lista != []:
+        print("Nombre: {0}".format(lista[indice]["nombre"]))
+        retorno = True
+    return retorno
+    
+
+def mostrar_estadisticas_jugador (lista:list, indice:str)->bool:
+    '''
+    - Muestra las estadisticas de un jugador.
+    - Recibe una lista y el indice del jugador a mostrar.
+    - Retorna False si la lista esta vacia sino retorna True.
+    '''
+    retorno = False
+    indice = int(indice)
+    jugador = lista[indice]
+    
+    if lista != []:
+        for clave in jugador["estadisticas"].keys():
+            print("{0} - {1}".format(clave, jugador["estadisticas"][clave]).capitalize(),end="\n")
+        
+        retorno = True
+        
+    return retorno
+
+
+mostrar_nombre_un_jugador(lista_jugadores, "0")
+mostrar_estadisticas_jugador (lista_jugadores, "0")
